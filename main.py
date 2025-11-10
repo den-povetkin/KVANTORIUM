33a8353b113b731f8fb594242b93b664b8b5b16a
den-povetkin <146918725+den-povetkin@users.noreply.github.com>
2025-11-10 11:46:48 +0800

    Create main.py
    primer

---
+++ main.py
@@ -0,0 +1,41 @@
+from turtle import *
+t1 = Turtle()
+t1.color('red')
+t1.pensize(10)
+def bukvaP():
+    t1.left(90)
+    t1.forward(100)
+    t1.right(90)
+    t1.forward(50)
+    t1.right(90)
+    t1.forward(100)
+    t1.penup()
+    t1.left(90)
+    t1.forward(30)
+    t1.pendown()
+    
+def bukvaR():
+    t1.left(90)
+    t1.forward(100)
+    t1.right(90)
+    t1.forward(50)
+    t1.right(90)
+    t1.forward(50)
+    t1.right(90)
+    t1.forward(50)
+    t1.penup()
+    t1.left(90)
+    t1.forward(50)
+    t1.left(90)
+    t1.forward(50)
+    t1.forward(10)
+
+    t1.pendown()
+
+bu = input ('Введите букву')
+if bu == 'п':
+    bukvaP()
+elif bu == 'р':
+    bukvaR()
+else:
+    print ('error')
