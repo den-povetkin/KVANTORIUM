9cd1415d0a368df4b50c2d972c59c45662940e85
den-povetkin <146918725+den-povetkin@users.noreply.github.com>
2025-12-18 12:19:16 +0800

    modified:   Servo.py

--- Servo.py
+++ Servo.py
@@ -5,22 +5,42 @@ motorL = AngularServo(24)
 #motorL = AngularServo(24, min_angle=-90, max_angle=90)
 #motorR = AngularServo(26, min_angle=-90, max_angle=90)
 def forvard():
+    """
+    Функция для движения вперед.
+    Устанавливает угол сервопривода motorL на 90 градусов.
+    """
     motorL.angle = 90
     #motorR.angle(-90)
     
 def back():
+    """
+    Функция для движения назад.
+    Устанавливает угол сервопривода motorL на -90 градусов.
+    """
     motorL.angle= - 90
     #motorR.angle(90)
     
 def left():
-    motorL.angle = 90 
+    """
+    Функция для поворота влево.
+    Устанавливает угол сервоприводов motorL и motorR на 90 градусов.
+    """
+    motorL.angle = 90
     motorR.angle = 90
     
 def right():
-    motorL.angle = -90 
+    """
+    Функция для поворота вправо.
+    Устанавливает угол сервоприводов motorL и motorR на -90 градусов.
+    """
+    motorL.angle = -90
     motorR.angle = -90
     
 def stop():
+    """
+    Функция для остановки.
+    Отключает сервопривод motorL.
+    """
     motorL.detach()
     #motorR.detach()
 
